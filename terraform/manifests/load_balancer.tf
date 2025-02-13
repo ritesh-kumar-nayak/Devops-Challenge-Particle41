fb4eca03dd1d21b824daf8ac9aec481e8973a33a
RiteshKumarNayak <riteshnayakgudu49@gmail.com>
2025-02-13 06:36:15 +0530

    attached public ip to lb instead of public subnet

--- terraform/manifests/load_balancer.tf
+++ terraform/manifests/load_balancer.tf
@@ -1,10 +1,10 @@
-# resource "azurerm_public_ip" "particle41-public-ip" {
-#   name                = var.lb_public_ip
-#   location            = azurerm_resource_group.particle41-rg.location
-#   resource_group_name = azurerm_resource_group.particle41-rg.name
-#   allocation_method   = "Static"
-#   sku                 = "Standard"
-# }
+resource "azurerm_public_ip" "particle41-public-ip" {
+  name                = var.lb_public_ip
+  location            = azurerm_resource_group.particle41-rg.location
+  resource_group_name = azurerm_resource_group.particle41-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
 
 resource "azurerm_lb" "particle41-lb" {
@@ -15,7 +15,9 @@ resource "azurerm_lb" "particle41-lb" {
 
   frontend_ip_configuration {
     name      = "PublicFrontend"
-    subnet_id = azurerm_subnet.public-subnet[var.selected_public_subnet_key].id # Deployed LB in Public Subnet
+    #subnet_id = azurerm_subnet.public-subnet[var.selected_public_subnet_key].id # Deployed LB in Public Subnet
+    public_ip_address_id = azurerm_public_ip.particle41-public-ip.id
+    # Since Load Balancers cannot be tied to subnets, we will deploy it with a Public IP instead.
   }
 }
 
